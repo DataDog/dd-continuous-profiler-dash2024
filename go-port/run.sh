796ff9252ef6c7caed680afc8e6839f523c3502e
Ivo Anjo <ivo.anjo@datadoghq.com>
2025-10-08 13:27:59 +0100

    Setup env for running

--- go-port/run.sh
+++ go-port/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Used in workshop environment, harmless for others
+export PATH=$PATH:/usr/lib/go-1.22/bin/
+
 DD_ENV=staging DD_SERVICE=movies-api-go DD_VERSION=`date +"%y%m%d_%H%M%S"` DD_PROFILING_ENABLED=true DD_PROFILING_EXECUTION_TRACE_PERIOD=1s go run main.go
